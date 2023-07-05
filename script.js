c4b528186bd61fa58da56cf577afa17cb92668cf
Miguel Munis <120065928+miguelmunis@users.noreply.github.com>
2023-07-05 10:06:50 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal('.dados1', { 
+    duration: 900, 
+    rotate: { x: 100, y: 0, z:0},
+});
+
+sr.reveal('.dados2', { 
+    duration: 1100, 
+    rotate: { x: 100, y: 0, z:0},
+});
+
+sr.reveal('.dados3', { 
+    duration: 1300, 
+    rotate: { x: 100, y: 0, z:0},
+});
+
+sr.reveal('#imgClock', { 
+    duration: 1300, 
+    rotate: { x: 0, y: 80, z:0},
+});
+
